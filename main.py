ba711aa94fdb51ad2ab12a952cbd215779a71b4e
Cybersport <Cybersport@26kadr.internal>
2024-05-29 11:56:42 +0300

    ssec

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from aiogram import Bot, Dispatcher, types, executor
 from config import TELEGRAM_TOKEN
 from keyboard.keyboards import get_keyboard_2, get_keyboard_1
-
+from keyboard.key_inline import inline_but_1, inline_but_2
 
 
 bot = Bot(token=TELEGRAM_TOKEN)
@@ -13,11 +13,11 @@ async def start(message: types.Message):
 
 @dp.message_handler(lambda message: message.text == 'Отправь фото кота')
 async def button_1_click(message: types.Message):
-    await bot.send_photo(message.chat.id, photo='https://avatars.mds.yandex.net/get-entity_search/2487574/434829330/orig', caption='Кот', reply_markup= get_keyboard_1())
+    await bot.send_photo(message.chat.id, photo='https://avatars.mds.yandex.net/get-entity_search/2487574/434829330/orig', caption='Кот', reply_markup= inline_but_1())
 
 @dp.message_handler(lambda message: message.text == 'Отправь фото машины')
 async def button_3_click(message: types.Message):
-    await bot.send_photo(message.chat.id, photo='https://autovogdenie.ru/wp-content/uploads/2022/05/foto-m4-csl_01.jpg', caption='Машина', reply_markup=get_keyboard_2())
+    await bot.send_photo(message.chat.id, photo='https://autovogdenie.ru/wp-content/uploads/2022/05/foto-m4-csl_01.jpg', caption='BMW MM4 CSL', reply_markup= inline_but_2())
 
 @dp.message_handler(lambda message: message.text == 'Перейти на следующую клавиатуру')
 async def button_2_click(message: types.Message):
